1a8c7f15e14275dcbe682b3293b18c818194eca7
mariam0825 <onafekomariam@gmail.com>
2026-08-18 14:51:03 +0100

    Fix dayOne: remove nested git repo, track as normal folder

---
+++ dayOne/javaScript/world.js
@@ -0,0 +1,7 @@
+for(const index = 1; index <= 6; index++){
+
+ console.log("\"Hello,World\"");
+
+}
+
+
---
+++ dayOne/python/name.py
@@ -0,0 +1,5 @@
+name = input("Enter your name : ")
+
+print("\"My name is \"" , name ,"\"","\"")
+
+print("\"Today is 17|08|2026\"")
